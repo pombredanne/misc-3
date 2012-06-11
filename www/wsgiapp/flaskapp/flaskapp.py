31e884b978d2417496032a01081d9ff05ac1a829
sakitake <k.take4.0709@gmail.com>
2012-06-11 11:53:10 +0900

    initial commit

---
+++ www/wsgiapp/flaskapp/flaskapp.py
@@ -0,0 +1,11 @@
+import sys
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return sys.version
+
+if __name__ == "__main__":
+    app.run()
---
+++ www/wsgiapp/flaskapp/flaskapp.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0, "D:\\home\\misc\\ec2\\www\\wsgiapp")
+from flaskapp.flaskapp import app as application
