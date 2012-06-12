6e67771b99e2cbf8099c975da78fe38a35f3677f
sakitake <k.take4.0709@gmail.com>
2012-06-12 10:22:07 +0900

    footerとか入れる。

--- www/wsgiapp/flaskapp/flaskapp.py
+++ www/wsgiapp/flaskapp/flaskapp.py
@@ -1,11 +1,14 @@
 import sys
+
 from flask import Flask
+from flask import render_template
+from version import VERSION
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return sys.version
+    return render_template('index.html', sys_version=sys.version, version=VERSION)
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
